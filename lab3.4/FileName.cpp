e172d1e4c9117a0eebb55ea295e0cc75d263fc0d
Карина Печерная <karina.pecherna.03@gmail.com>
2024-09-23 08:42:51 +0300

    ydd

---
+++ lab3.4/FileName.cpp
@@ -0,0 +1,29 @@
+﻿// Lab_03_4.cpp
+// < Печерна Каріна >
+// Лабораторна робота № 3.4
+// Розгалуження, задане плоскою фігурою.
+// Варіант 0.1
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    double x; // вхідний аргумент
+    double y; // вхідний параметр
+    double R; // вхідний параметр
+
+    cout << "x = "; cin >> x;
+    cout << "y = "; cin >> y;
+    cout << "R = "; cin >> R;
+
+    // Перша чверть (частина кола)
+    if ((x >= 0 && y >= 0 ) ||
+        (x >= 0 && y >= 0 && y >= -x - R))
+        cout << "yes" << endl;
+    else 
+        cout << "no" << endl;
+        
+        
+    cin.get();
+    return 0;
+}
